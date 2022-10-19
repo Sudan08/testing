8ccec67523550a914bffa901165e6f8341cb753e
snishant011 <shresthanishant011@gmail.com>
2022-10-19 23:14:12 +0545

    closes #10
    implemented shortcut key for toggling dark mode

---
+++ src/app/providers/ShortcutProvider.tsx
@@ -0,0 +1,29 @@
+import { useColorMode } from '@chakra-ui/react';
+import { useCallback, useEffect, FC, PropsWithChildren } from 'react';
+
+const ShortcutProvider: FC<PropsWithChildren> = ({ children }) => {
+  const { toggleColorMode } = useColorMode();
+  const handleKeyPress = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.shiftKey) {
+        if (e.code === 'KeyD' && document?.activeElement?.tagName !== 'INPUT') {
+          toggleColorMode();
+        }
+      }
+    },
+    [toggleColorMode]
+  );
+
+  useEffect(() => {
+    if (window !== undefined) {
+      document.addEventListener('keypress', handleKeyPress);
+    }
+    return () => {
+      document.removeEventListener('keypress', handleKeyPress);
+    };
+  }, [handleKeyPress]);
+
+  return <div>{children}</div>;
+}
+
+export default ShortcutProvider
